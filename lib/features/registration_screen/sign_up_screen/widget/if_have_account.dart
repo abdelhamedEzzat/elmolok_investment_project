5f52cc5ceb028c067d27bd1fd0477a9dfa39141e
abdelhamid <amabdo275@gmail.com>
2024-03-28 00:22:57 +0200

    refactoring color from singIn Word

--- lib/features/registration_screen/sign_up_screen/widget/if_have_account.dart
+++ lib/features/registration_screen/sign_up_screen/widget/if_have_account.dart
@@ -1,3 +1,4 @@
+import 'package:elmolak_investment_app/core/constans/colors/color_manger.dart';
 import 'package:elmolak_investment_app/core/constans/strings/strings_manger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -18,7 +19,7 @@ class IfYouHaveAccount extends StatelessWidget {
           Text(
             StringsManger.signIn,
             style: TextStyle(
-                color: Colors.black,
+                color: ColorManger.buttonColor,
                 fontSize: 14.w,
                 fontWeight: FontWeight.bold),
           ),

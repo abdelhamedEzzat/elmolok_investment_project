c1507a78bad732473e508a6a0143280d4e4ee8d0
abdelhamid <amabdo275@gmail.com>
2024-03-29 00:21:12 +0200

    create route to singn up screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:elmolak_investment_app/features/intro_screens/intro_screens.dart';
+import 'package:elmolak_investment_app/features/registration_screen/sign_up_screen/signup_screen.dart';
 import 'package:elmolak_investment_app/features/splash_screen/splash_screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
           // Define your home screen route here
           routes: {
             '/FirstIntroScreen': (context) => const FirstIntroScreen(),
+            '/SignUpScreen': (context) => const SignUpScreen(),
           },
         );
       },
